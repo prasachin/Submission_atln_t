0a192349354921efe66463c71734a678c652a854
sachin <sachinp@iitbhilai.ac.in>
2025-03-31 11:11:07 +0530

    version conflict fix

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ["react-ace", "brace"],
+  build: {
+    rollupOptions: {
+      external: ["react"],
+    },
   },
-  esbuild: {
-    jsxInject: 'import React from "react";',
+  optimizeDeps: {
+    include: ["react-csv"],
   },
 });
